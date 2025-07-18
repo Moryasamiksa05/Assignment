9edcd6eb7e19a71bf55b5ad4cf8bd7cbe00dccaf
SamikshaMorya <moryasamiksha52@gmail.com>
2025-07-18 10:44:38 +0530

    api added

--- Test-Assignment/src/api.js
+++ Test-Assignment/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // replace with your deployed backend URL later
+  baseURL: 'https://assignment-8hzp.onrender.com/api', 
 });
 
 export default API;
